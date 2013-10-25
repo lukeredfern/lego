c42aa6aedfd19f21024db27ff3cbc1cca9d63144
luke <lukeredfern92@googlemail.com>
2013-10-25 10:47:19 +0100

    bend test

---
+++ MATLAB/Bend Test/data_proccessing.m
@@ -0,0 +1,12 @@
+
+
+% Constants
+SpringOriginalLength=0.03;  % m
+SpringConstant=166;         % N/m
+BeamWidth=0.03;             % m
+BeamDepth=0.001;            % m
+
+
+% Prelim Calcs
+I=(BeamDepth^3*BeamWidth)/12;
+
